1e4535435e5f939f8c0b8c8326d88e644ff6b529
Parth Mevawla <parthmevawala@gmail.com>
2022-04-18 11:47:38 +0530

    logger implemented

--- src/middleware/authentication.middleware.js
+++ src/middleware/authentication.middleware.js
@@ -5,30 +5,34 @@ const { ER_UNAUTHENTICATED_USER } = require("../constants/errors.constants");
 const { execute } = require("../includes/database_connection");
 const { decodeToken } = require("../helpers/jwt");
 const { USERS } = require("../constants/tables.constants");
+const logger = require("../helpers/logger");
 
 // METHODS ==================================================================================================
 /**
  * Authentication middleware function.
- * @param {object} req 
- * @param {object} res 
- * @param {object} next 
- * @returns 
+ * @param {object} req
+ * @param {object} res
+ * @param {object} next
+ * @returns
  */
 const authentication = async (req, res, next) => {
 	try {
-        let token = req.headers.authorization.replace("Bearer ", "");
-        const dToken = decodeToken(token);
-        
-        // If token is decoded then it will have id. Else it will throw error
-        if(dToken.id) {
-            const records = await execute(`SELECT id FROM ${USERS} WHERE token='${token}'`);
-            if(records.rowCount === 1) {
-                req._id = records.rows[0].id;
-                return next();
-            }
-        }
+		let token = req.headers.authorization.replace("Bearer ", "");
+		const dToken = decodeToken(token);
 
-        // If the code comes here then there is some issue in decoding token. Hence, user is not authenticated.
+		// If token is decoded then it will have id. Else it will throw error
+		if (dToken.id) {
+			const records = await execute(
+				`SELECT id FROM ${USERS} WHERE token='${token}'`,
+			);
+			if (records.rowCount === 1) {
+				req._id = records.rows[0].id;
+				return next();
+			}
+		}
+
+		// If the code comes here then there is some issue in decoding token. Hence, user is not authenticated.
+		logger.error(ER_UNAUTHENTICATED_USER.code);
 		res.status(ER_UNAUTHENTICATED_USER.statusCode).send({
 			errorCode: ER_UNAUTHENTICATED_USER.code,
 			statusCode: ER_UNAUTHENTICATED_USER.statusCode,
@@ -36,8 +40,8 @@ const authentication = async (req, res, next) => {
 			data: {},
 		});
 	} catch (error) {
-        console.log(error);
-        // If there is any error then user is not authenticated.
+		// If there is any error then user is not authenticated.
+		logger.error(ER_UNAUTHENTICATED_USER.code);
 		res.status(ER_UNAUTHENTICATED_USER.statusCode).send({
 			errorCode: ER_UNAUTHENTICATED_USER.code,
 			statusCode: ER_UNAUTHENTICATED_USER.statusCode,
