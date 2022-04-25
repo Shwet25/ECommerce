3accc10184ac7a796278b56c5d6ccbd865425259
volleyadi415 <jhaa241@gmail.com>
2022-04-25 13:08:06 +0530

    changes done

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -3,9 +3,9 @@
 // IMPORTS ==================================================================================================
 const { USERS } = require("../constants/tables.constants");
 const {
-	ER_DATA_ALREADY_EXISTS,
-	ER_DATA_NOT_FOUND,
-	ER_USER_BLOCKED,
+    ER_DATA_ALREADY_EXISTS,
+    ER_DATA_NOT_FOUND,
+    ER_USER_BLOCKED,
 } = require("../constants/errors.constants");
 const { generateToken } = require("../helpers/jwt");
 
@@ -16,14 +16,14 @@ const { generateToken } = require("../helpers/jwt");
  * @returns
  */
 const getAllUsers = async (con) => {
-	const data = await con.execute(
-		`SELECT id, firstName, lastName, email, contact FROM ${USERS} ORDER BY id ASC`,
-	);
+    const data = await con.execute(
+        `SELECT id, firstName, lastName, email, contact FROM ${USERS} ORDER BY id ASC`
+    );
 
-	return {
-		data: data.rows || data,
-		message: "All users listed.",
-	};
+    return {
+        data: data.rows || data,
+        message: "All users listed.",
+    };
 };
 
 /**
@@ -33,33 +33,33 @@ const getAllUsers = async (con) => {
  * @returns
  */
 const addUser = async (con, body) => {
-	const response = {
-		message: "User registered.",
-	};
+    const response = {
+        message: "User registered.",
+    };
 
-	const { firstName, lastName, contact, password, email } = body;
+    const { firstName, lastName, contact, password, email } = body;
 
-	// Check for existing email
-	if (
-		(await con.execute(`SELECT id FROM ${USERS} WHERE email = '${email}'`))
-			.rowCount > 0
-	)
-		throw ER_DATA_ALREADY_EXISTS("email");
+    // Check for existing email
+    if (
+        (await con.execute(`SELECT id FROM ${USERS} WHERE email = '${email}'`))
+            .rowCount > 0
+    )
+        throw ER_DATA_ALREADY_EXISTS("email");
 
-	// Insert operation
-	const data = await con.execute(
-		`INSERT INTO ${USERS} 
+    // Insert operation
+    const data = await con.execute(
+        `INSERT INTO ${USERS} 
 		(email, password, firstName${lastName ? ", lastName" : ""}${
-			contact ? ", contact" : ""
-		}) 
+            contact ? ", contact" : ""
+        }) 
 		VALUES ('${email}', '${password}', '${firstName}'${
-			lastName ? `, '${lastName}'` : ""
-		}${contact ? `, '${contact}'` : ""}) 
-		RETURNING id, firstName, lastName, contact, email, blocked`,
-	);
-	response.data = data.rows || data;
+            lastName ? `, '${lastName}'` : ""
+        }${contact ? `, '${contact}'` : ""}) 
+		RETURNING id, firstName, lastName, contact, email, blocked`
+    );
+    response.data = data.rows || data;
 
-	return response;
+    return response;
 };
 
 /**
@@ -69,45 +69,45 @@ const addUser = async (con, body) => {
  * @returns
  */
 const userLogin = async (con, body) => {
-	const response = {
-		message: "User has been logged in.",
-	};
-	const { email, password } = body;
+    const response = {
+        message: "User has been logged in.",
+    };
+    const { email, password } = body;
 
-	// Check user if exists.
-	let records = await con.execute(
-		`SELECT id, password, blocked FROM ${USERS} WHERE email='${email}'`,
-	);
+    // Check user if exists.
+    let records = await con.execute(
+        `SELECT id, password, blocked FROM ${USERS} WHERE email='${email}'`
+    );
 
-	// if records are found then proceed
-	if (records.rowCount > 0) {
-		const record = records.rows[0];
+    // if records are found then proceed
+    if (records.rowCount > 0) {
+        const record = records.rows[0];
 
-		// Check if user is blocked
-		if (record.blocked) throw ER_USER_BLOCKED;
+        // Check if user is blocked
+        if (record.blocked) throw ER_USER_BLOCKED;
 
-		// Check password
-		if (record.password !== password) {
-			//**** ADD ENCRYPTION HERE LATER ON ****
-			throw ER_DATA_NOT_FOUND("user");
-		}
+        // Check password
+        if (record.password !== password) {
+            //**** ADD ENCRYPTION HERE LATER ON ****
+            throw ER_DATA_NOT_FOUND("user");
+        }
 
-		// Generate and update token.
-		const token = generateToken(record.id);
-		response.data = (
-			await con.execute(
-				`UPDATE ${USERS} 
+        // Generate and update token.
+        const token = generateToken(record.id);
+        response.data = (
+            await con.execute(
+                `UPDATE ${USERS} 
 				SET token='${token}' 
 				WHERE id=${record.id} 
-				RETURNING id, firstName, lastName, contact, email, token`,
-			)
-		).rows[0];
+				RETURNING id, firstName, lastName, contact, email, token`
+            )
+        ).rows[0];
 
-		return response;
-	}
+        return response;
+    }
 
-	// else error
-	throw ER_DATA_NOT_FOUND("user");
+    // else error
+    throw ER_DATA_NOT_FOUND("user");
 };
 
 /**
@@ -117,65 +117,35 @@ const userLogin = async (con, body) => {
  * @returns
  */
 const userLogout = async (con, id) => {
-	const response = {
-		message: "User successfully logged out.",
-		data: {},
-	};
+    const response = {
+        message: "User successfully logged out.",
+        data: {},
+    };
 
-	await con.execute(`UPDATE ${USERS} SET token='' WHERE id=${id}`);
-	return response;
+    await con.execute(`UPDATE ${USERS} SET token='' WHERE id=${id}`);
+    return response;
 };
 
 /**
- * User Update Password - service
+ * delete user - service
  * @param {object} con
- * @param {object} body
+ * @param {string} body
  * @returns
  */
-const userUpdatePassword = async (con, body) => {
-	const response = {
-		message: "Password successfully updated.",
-	};
+const deleteUser = async (con, body) => {
+    const response = {
+        message: "User delete successfully.",
+        data: {},
+    };
 
-	response.data = await con.execute(`
-		UPDATE ${USERS}
-		SET password = '${body.password}', token = ''
-		WHERE email = '${body.email}'
-		RETURNING id, firstName, lastName, contact, email
-	`);
+    const { id } = body;
+    const data = await con.execute(
+        `DELETE FROM ${USERS} WHERE id=${id} RETURNING id`
+    );
 
-	if (response.data.rowCount === 0) throw ER_DATA_NOT_FOUND("user");
+    response.data = data.rows || data;
 
-	return response;
+    return response;
 };
-
-/**
- * Delete user - service
- * @param {object} con
- * @param {string} id
- * @returns
- */
-const deleteUser = async (con, id) => {
-	const response = {
-		message: "User delete successfully.",
-		data: {},
-	};
-;
-	const data = await con.execute(
-		`DELETE FROM ${USERS} WHERE id=${id} RETURNING id`,
-	);
-
-	response.data = data.rows || data;
-
-	return response;
-};
-
 // EXPORTS ==================================================================================================
-module.exports = {
-	getAllUsers,
-	addUser,
-	userLogin,
-	userLogout,
-	userUpdatePassword,
-	deleteUser,
-};
+module.exports = { getAllUsers, addUser, userLogin, userLogout, deleteUser };
